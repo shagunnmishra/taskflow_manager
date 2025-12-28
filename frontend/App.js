894f966ff646835037f849f43e5ae598ee4699f4
Shagun <shagun4kashyap@gmail.com>
2025-12-28 19:25:54 +0530

    Create App component for TaskFlow Manager

---
+++ frontend/App.js
@@ -0,0 +1,13 @@
+// frontend/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>TaskFlow Manager</h1>
+      <p>Add and view your tasks here!</p>
+    </div>
+  );
+}
+
+export default App;
